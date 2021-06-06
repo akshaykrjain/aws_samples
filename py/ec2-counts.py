0e3976be1f93e73b9ca4ac6202087c3298a3f8b0
akshaykrjain <akshayjain.ecb@gmail.com>
2021-06-06 09:32:47 +0200

    Fixed CFT Output

---
+++ py/ec2-counts.py
@@ -0,0 +1,13 @@
+#TODO
+import boto3
+
+print("hello AWS!")
+
+
+# Let's use Amazon S3
+s3 = boto3.resource('s3')
+
+# Print out bucket names
+for bucket in s3.buckets.all():
+    print(bucket.name)
+
