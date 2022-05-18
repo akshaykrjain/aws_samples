6f7d24871ef5f7832825e7bb3eddecaad4dc7955
akshaykrjain <akshayjain.ecb@gmail.com>
2022-05-18 17:50:44 +0200

    ubuntu bump in ec2 terraform

--- tf/ec2/main.tf
+++ tf/ec2/main.tf
@@ -1,6 +1,15 @@
 variable "key_pair_name" {
   type    = string
-  default = "akshay"
+  default = "NEP-MAC"
+}
+
+data "cloudinit_config" "server_config" {
+  gzip          = true
+  base64_encode = true
+  part {
+    content_type = "text/cloud-config"
+    content      = file("${path.module}/server.yaml")
+  }
 }
 
 data "aws_ami" "ubuntu" {
@@ -8,7 +17,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-arm64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-server-*"]
   }
 
   filter {
@@ -27,18 +36,10 @@ data "http" "myip" {
   url = "http://ipv4.icanhazip.com"
 }
 
-resource "aws_security_group" "allow_ssh" {
+resource "aws_security_group" "web_server" {
   name        = "allow_tls"
   description = "Allow SSH inbound traffic"
   vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    description = "SSH from My IP"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
-  }
 
   egress {
     from_port        = 0
@@ -49,20 +50,41 @@ resource "aws_security_group" "allow_ssh" {
   }
 
   tags = {
-    Name = "allow_ssh_my_ip"
+    Name = "web_server"
   }
+}
+
+resource "aws_security_group_rule" "ssh_allow" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["${chomp(data.http.myip.body)}/32"]
+  security_group_id = aws_security_group.web_server.id
+}
+
+resource "aws_security_group_rule" "http_allow" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.web_server.id
 }
 
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t4g.micro"
+  instance_type          = "t4g.medium"
   key_name               = var.key_pair_name
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.web_server.id]
+  user_data              = data.cloudinit_config.server_config.rendered
+  user_data_replace_on_change = true
   tags = {
-    Name = "Test"
+    Name = "WebServer-Ubuntu"
   }
 }
 
-output "ssh_command" {
-  value = "ssh -i ~/.ssh/${var.key_pair_name}.pem ubuntu@${aws_instance.web.public_ip}"
+output "WEB_ADDRESS" {
+  value = "http://${aws_instance.web.public_ip}"
 }
---
+++ tf/ec2/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.14.0"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "2.2.0"
+    }
+  }
+}
+
+
