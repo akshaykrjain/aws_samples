5bc49c6f1c5beaecb679125330d25a935beb319b
akshaykrjain <akshayjain.ecb@gmail.com>
2022-05-23 13:27:05 +0200

    fix provider aws

---
+++ tf/tfc/versions.tf
@@ -0,0 +1,23 @@
+terraform {
+
+  cloud {
+    organization = "akshaykrjain"
+
+    workspaces {
+      name = "learn-terraform-cloud"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
