7cc2da199a15b4983ecd787e08045f216ff1e2b9
Akshay Jain <akshayjain.ecb@gmail.com>
2022-05-31 13:42:24 +0200

    Example for INTELLIGENT_TIERING in S3

--- tf/s3/s3.tf
+++ tf/s3/s3.tf
@@ -1,3 +1,35 @@
 resource "aws_s3_bucket" "bucket" {
-    bucket_prefix = "created-with-terraform"
+  bucket_prefix = "created-with-terraform"
 }
+
+
+resource "aws_s3_bucket_intelligent_tiering_configuration" "entire-bucket" {
+  bucket = aws_s3_bucket.bucket.bucket
+  name   = "EntireBucket"
+
+  tiering {
+    access_tier = "DEEP_ARCHIVE_ACCESS"
+    days        = 180
+  }
+  tiering {
+    access_tier = "ARCHIVE_ACCESS"
+    days        = 125
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "intelligent" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    id = "transition_all_object_to_INTELLIGENT_TIERING"
+
+    transition {
+      days          = 7
+      storage_class = "INTELLIGENT_TIERING"
+    }
+
+    status = "Enabled"
+  }
+}
+
+
