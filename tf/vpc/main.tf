86a319b6d0ca7af480c1d50607602c8f5ff43530
Akshay Jain <akshayjain.ecb@gmail.com>
2022-06-29 16:56:24 +0200

    added VPC terraform exmaple

---
+++ tf/vpc/main.tf
@@ -0,0 +1,21 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public_1"
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "public_2"
+  }
+}
