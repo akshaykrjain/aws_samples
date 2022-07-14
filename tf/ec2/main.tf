fa78f2993f8035589bbf080df89a17583444c1aa
Akshay Jain <akshayjain.ecb@gmail.com>
2022-07-14 14:58:11 +0200

    amazon linux ami data filter

--- tf/ec2/main.tf
+++ tf/ec2/main.tf
@@ -12,20 +12,33 @@ data "cloudinit_config" "server_config" {
   }
 }
 
+##UBUNTU AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
-
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-server-*"]
   }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
   owners = ["099720109477"] # Canonical
+}
+
+## USE BELOW FOR Amazon Linux 2 AMI
+
+data "aws_ami" "amzn2" {
+  most_recent = true
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2*arm*64-gp2"]
+  }
+  owners = ["amazon"]
 }
 
 data "aws_vpc" "default" {
